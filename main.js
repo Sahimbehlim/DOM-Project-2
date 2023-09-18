e66b9cd8f425f5ba0e24a3918a98c5403625cfa8
Sahim Behlim <126802461+Sahimbehlim@users.noreply.github.com>
2023-09-18 08:49:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var container = document.querySelector("#container");
+var icon = document.querySelector("i");
+
+container.addEventListener("dblclick",function(){
+    icon.style.transform = "translate(-50%,-50%) scale(1)";
+    icon.style.opacity = 0.8;
+    icon.style.color = "red";
+    setTimeout(function(){
+        icon.style.opacity = 0;
+    },1000)
+})
